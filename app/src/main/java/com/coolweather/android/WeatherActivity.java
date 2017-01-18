e2e86a23791458080d9ce63953cc06a6b7f61882
Tony <tony@gmail.com>
2017-01-18 14:18:27 +0800

    First commit.

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+        import android.content.Intent;
         import android.content.SharedPreferences;
         import android.graphics.Color;
         import android.os.Build;
@@ -21,6 +22,7 @@ package com.coolweather.android;
         import com.bumptech.glide.Glide;
         import com.coolweather.android.gson.Forecast;
         import com.coolweather.android.gson.Weather;
+        import com.coolweather.android.service.AutoUpdateService;
         import com.coolweather.android.util.HttpUtil;
         import com.coolweather.android.util.Utility;
 
@@ -217,5 +219,7 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
 
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
